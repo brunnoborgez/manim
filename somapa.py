c956f5ec78bff50157e013754907ddffaae8142f
brunnoborgez <brunnoborgez@gmail.com>
2022-01-07 13:24:20 -0300

    segunda versao

--- somapa.py
+++ somapa.py
@@ -2,8 +2,8 @@ from manim import *
 
 class Soma(Scene):
     def construct(self):
-
+        
+        titulo = Title('Soma dos n termos de uma progressão aritmética')
         formula = MathTex('S_{n}','=', 'a_1', '+', 'a_2', '+', '\dots', '+', 'a_{n-1}', '+', 'a_n')
-
-
+    
         self.add(formula)
