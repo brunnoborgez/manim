d81c6c7d162df93d6a76c5c7e294ebf864f9afed
Alexandre Oliveira da Silva <56375513+alexandre-sollis@users.noreply.github.com>
2022-01-07 23:02:35 -0400

    Update somapa.py
    Atualizações no código descritas no próprio código!

--- somapa.py
+++ somapa.py
@@ -1,9 +1,15 @@
 from manim import *
 
 class Soma(Scene):
+    
     def construct(self):
         
         titulo = Title('Soma dos n termos de uma progressão aritmética')
-        formula = MathTex('S_{n}','=', 'a_1', '+', 'a_2', '+', '\dots', '+', 'a_{n-1}', '+', 'a_n')
+        
+        formula = MathTex('S_{n}','=', 'a_1', '+', 'a_2', '+', '\dots', '+', 'a_{n-1}', '+', 'a_n', font_size = 70) #tamanho da fonte da alterada!
     
-        self.add(formula)
+        #self.add(formula)
+        
+        self.play(Write(formula[0:11]), run_time = 3) #linha adicionada para escrever a fórmula!
+       
+        #alterações feitas: tamanho da fonte alterado, linha 11 excluída, linha 13 adicionada!
